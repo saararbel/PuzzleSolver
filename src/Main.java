ea230455a13060802ba9c990e542f656062b7b38
saararbel <saararbel1@gmail.com>
2018-05-04 10:45:38 +0300

    fsdfds

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,24 @@ import java.util.Optional;
 
 public class Main {
 	
+	public static void p(int cell) {
+		int columns = 4;
+		int realCellRow = ((double)cell/columns) > (double)(cell/columns) ? cell/columns : (cell/columns) - 1;
+		int realCellColumn = (cell % columns) == 0 ? columns - 1 : (cell % columns)  - 1;
+		
+		System.out.println("cell " + cell + " position is: (" + realCellRow + "," + realCellColumn + ")");
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Puzzle puzzle = new PuzzleBuilder().parseInputFile();
 		PuzzleSolver puzzleSolver = new PuzzleSolver();
 		
-		Optional<SolutionData> solution = puzzleSolver.bfs(puzzle);
+		Optional<SolutionData> solution = puzzleSolver.aStar(puzzle);
 		if(solution.isPresent()) {
 			puzzleSolver.printSolution(solution.get().getEndState(), solution.get().getNumPopedFromOpenList());
 		}
+//		for(int i=1 ; i<=11 ; i++) {
+//			p(i);			
+//		}
 	}
 }
--- src/PuzzleSolver.java
+++ src/PuzzleSolver.java
@@ -1,6 +1,8 @@
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Optional;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
@@ -39,9 +41,8 @@ public class PuzzleSolver {
 			for(int i=0 ; i<4 ; i++) {
 				if(!possibleActions[i].equals(PuzzleState.EMPTY_ACTION)) {
 					PuzzleState tempState = headState.preformAction(possibleActions[i]);
-					if(!closedList.contains(tempState)) {
+					if(!closedList.contains(tempState) && !openList.contains(tempState)) {
 						if(puzzle.isEndState(tempState)) {
-							// Finish!
 							return Optional.of(new SolutionData(tempState, numPopedFromOpenList));
 						}
 						openList.add(tempState);
@@ -53,4 +54,100 @@ public class PuzzleSolver {
 		return Optional.empty();
 	}
 	
+	
+	
+	private int calcFn(PuzzleState state) {
+		return gn(state) + hn(state);
+	}
+	
+	/**
+	 * This function iterate ofer state fathers untill it is null (which is the start state)
+	 * @param state
+	 * @return
+	 */
+	private int gn(PuzzleState state) {
+		int counter = 0;
+		while(state.getFather() != null) {
+			state = state.getFather();
+			counter++;
+		}
+		
+		return counter;
+	}
+	
+	
+
+	/**
+	 * This function calcs the estimated distance from state to endState - 
+	 * The function calcs the 
+	 * @param endState
+	 * @param state
+	 * @return
+	 */
+	private int hn(PuzzleState state) {
+		int dist = 0;
+		int[][] stateBoardValues = state.getBoardValues();
+		int rows = stateBoardValues.length;
+		int columns = stateBoardValues[0].length;
+		
+		for(int i=0 ; i<rows ; i++) {
+			for(int j=0 ; j<columns ; j++) {
+				int cell = stateBoardValues[i][j];
+				/**
+				 * This part of code calculates the TRUE place of the cell in the final end state , this
+				 * helps us to calculate the Manhattan distance
+				 */
+				int realCellRow = ((double)cell/columns) > (double)(cell/columns) ? cell/columns : (cell/columns) - 1;
+				int realCellColumn = (cell % columns) == 0 ? columns - 1 : (cell % columns)  - 1;
+				
+				dist += Math.abs(i-realCellRow) + Math.abs(j-realCellColumn);
+			}
+		}
+		
+		return dist;
+	}
+
+	public Optional<SolutionData> aStar(Puzzle puzzle) {
+		int numPopedFromOpenList = 0;
+		Set<PuzzleState> closedList = new HashSet<>();
+		Queue<PuzzleState> openList = new PriorityQueue<>(new Comparator<PuzzleState>() {
+
+			@Override
+			public int compare(PuzzleState state0, PuzzleState state1) {
+				return calcFn(state0) - calcFn(state1);
+			}
+		});
+		
+		openList.add(puzzle.getStartState());
+		
+		while(!openList.isEmpty()) {
+			PuzzleState headState = openList.poll();
+			numPopedFromOpenList++;
+			
+			if(puzzle.isEndState(headState)) {
+				return Optional.of(new SolutionData(headState, numPopedFromOpenList));
+			}
+			
+			closedList.add(headState);
+			String[] possibleActions = headState.getPossibleActions();
+			for(int i=0 ; i<4 ; i++) {
+				if(!possibleActions[i].equals(PuzzleState.EMPTY_ACTION)) {
+					PuzzleState tempState = headState.preformAction(possibleActions[i]);
+					if(!closedList.contains(tempState) && !openList.contains(tempState)) {
+						openList.add(tempState);
+					}
+				}
+			}
+		}
+		
+		
+		
+		return Optional.empty();
+	}
+	
+	public Optional<SolutionData> dfid(Puzzle puzzle) {
+		
+		
+		return null;
+	}
 }
